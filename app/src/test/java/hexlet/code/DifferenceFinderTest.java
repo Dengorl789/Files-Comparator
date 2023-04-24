cbbce4646eb2cbdb27fc7dbec2ad35a3fda30a42
Denis Gorlov <dengorl1998@gmail.com>
2023-04-24 18:51:00 +0300

    Tests for Parser and DifferenceFinder classes added

---
+++ app/src/test/java/hexlet/code/DifferenceFinderTest.java
@@ -0,0 +1,50 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class DifferenceFinderTest {
+    public final Map<String, Object> map1 = Map.of(
+            "intKey1", 1,
+            "stringKey", "abc",
+            "charKey", "a"
+
+    );
+    public final Map<String, Object> map2 = Map.of(
+            "intKey2", 2,
+            "stringKey", "cba",
+            "charKey", "a"
+
+    );
+    public final Set<String> keySet = new  TreeSet<>(map1.keySet());
+
+    @Test
+    public void differenceFinderTest() {
+        var actual = DifferenceFinder.differ(map1, map2);
+        List<Map<String, Object>> expected = new ArrayList<>();
+        Map<String, Object> firstMap = new LinkedHashMap<>();
+        firstMap.put("key", "intKey1");
+        firstMap.put("oldValue", map1.get("intKey1"));
+        firstMap.put("status", "removed");
+        Map<String, Object> secondMap = new LinkedHashMap<>();
+        secondMap.put("key", "intKey2");
+        secondMap.put("newValue", map2.get("intKey2"));
+        secondMap.put("status", "added");
+        Map<String, Object> thirdMap = new LinkedHashMap<>();
+        thirdMap.put("key", "stringKey");
+        thirdMap.put("oldValue", map1.get("stringKey"));
+        thirdMap.put("newValue", map2.get("stringKey"));
+        thirdMap.put("status", "updated");
+        Map<String, Object> fourthMap = new LinkedHashMap<>();
+        fourthMap.put("key", "charKey");
+        fourthMap.put("oldValue", map1.get("charKey"));
+        fourthMap.put("status", "unchanged");
+        expected.add((fourthMap));
+        expected.add(firstMap);
+        expected.add(secondMap);
+        expected.add(thirdMap);
+        Assertions.assertEquals(expected, actual);
+    }
+}
--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -1,14 +1,40 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ParserTest {
-    @Test
-    public void chooseFormatTest() throws IOException {
-        var actual = Parser.parser("data1", "json");
+    private final String path = "src/test/resources/expected/testParser.json";
+    private final Path pathParser = Paths.get(path);
+    private final String data;
 
+    {
+        try {
+            data = Files.readString(pathParser);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void parserTest() throws IOException {
+        var actual = Parser.parser(data, "json");
+        TreeMap<String, Object> expected = new TreeMap<>();
+        expected.put("intKey", 1);
+        expected.put("charKey", 'a');
+        expected.put("stringKey", "abc");
+        expected.put("nullKey", null);
+        expected.put("doubleKey", 0.5);
+        expected.put("booleanKey", true);
+        Assertions.assertEquals(expected, actual);
     }
 }
