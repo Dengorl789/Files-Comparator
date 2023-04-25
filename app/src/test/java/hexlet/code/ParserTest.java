dea7484678e0b0d2c391efed277d5745af2e07a0
Denis Gorlov <dengorl1998@gmail.com>
2023-04-25 20:14:08 +0300

    Parser class tests updated 2

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -15,23 +15,41 @@ import java.util.TreeMap;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
-    private final String path = "src/test/resources/testParser.json";
-    private final Path pathParser = Paths.get(path);
-    private final String data;
+    private final String path1 = "src/test/resources/testParser.json";
+    private final String path2 = "src/test/resources/testParser.yml";
+    private final Path pathParserJson = Paths.get(path1);
+    private final Path pathParserYml = Paths.get(path2);
+    private final String data1;
+    private final String data2;
     private final int intNumber = 1;
     private final double doubleNumber = 0.5;
 
     {
         try {
-            data = Files.readString(pathParser);
+            data1 = Files.readString(pathParserJson);
+            data2 = Files.readString(pathParserYml);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    public void parserTest() throws IOException {
-        var actual = Parser.parser(data, "json");
+    public void parserTestJson() throws IOException {
+        var actual = Parser.parser(data1, "json");
+        TreeMap<String, Object> expected = new TreeMap<>();
+        expected.put("intKey", intNumber);
+        expected.put("charKey", 'a');
+        expected.put("stringKey", "abc");
+        expected.put("nullKey", null);
+        expected.put("doubleKey", doubleNumber);
+        expected.put("booleanKey", true);
+        var actualString = actual.toString();
+        var expectedString = expected.toString();
+        Assertions.assertEquals(expectedString, actualString);
+    }
+    @Test
+    public void parserTestYml() throws IOException {
+        var actual = Parser.parser(data2, "yml");
         TreeMap<String, Object> expected = new TreeMap<>();
         expected.put("intKey", intNumber);
         expected.put("charKey", 'a');
